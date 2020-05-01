192d7998f3d0878df059d73a750314548a542032
BOTREL Kilian <botrel.kilian@gmail.com>
2020-05-01 16:49:55 +0200

    Added more checks

--- tools/Polynom.py
+++ tools/Polynom.py
@@ -1,3 +1,4 @@
+import re
 from collections import Counter
 from tools.utils import print_error, lookup_index
 
@@ -10,20 +11,26 @@ class Polynom:
 		self.factor = factor
 
 	def check(self):
-		error = False
 		count = Counter(self.definition)
-
+		numbers = len(re.findall('[^X\^ *]+', self.definition))
 		if count['X'] > 1 or count['*'] > 1 or count['^'] > 1:
 			print_error(self.definition, 0, 0, 2)
 
 		self.x = True if count['X'] == 1 else False
 		if self.x == True :
+			if count['*'] == 0 and count['^'] == 1 and numbers > 2:
+				print_error(self.definition, 0, 0, 2)
+			if count['*'] == 1 and count['^'] == 0 and numbers > 1:
+				print_error(self.definition, 0, 0, 2)
+			if count['*'] == 0 and count['^'] == 0 and numbers > 1:
+				print_error(self.definition, 0, 0, 2)
 			index = lookup_index(self.definition, '^')
 			index2 = lookup_index(self.definition, 'X')
 			if index > 0 and (index2 == -1 or index2 != index - 1):
-				error = True
-		if error == True :
-			print_error(self.definition, 0, 0, 2)
+				print_error(self.definition, 0, 0, 2)
+		else:
+			if count['^'] > 0 or count['*'] > 0 or numbers > 1:
+				print_error(self.definition, 0, 0, 2)
 
 	def display(self):
 		print(f"sign : {self.sign}\npower : {self.power}\nx : {self.x}\nfactor : {self.factor}\n")
